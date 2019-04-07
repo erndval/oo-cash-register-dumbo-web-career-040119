85cc632ed4d5021ac81a7e234738057723b781a2
github username <github email address>
2019-04-07 15:21:16 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+require 'pry'
 
+class CashRegister
+  attr_reader :discount
+  attr_accessor :total
+
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @cart = []
+    @total_array = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total_array << price.to_f * quantity.to_f
+    quantity.times {@cart << title}
+    @total = @total_array.sum
+  end
+
+  def apply_discount
+    if @discount > 0
+      @total = @total - @total*@discount.to_f/100
+      "After the discount, the total comes to $#{@total.to_i}."
+  else
+      "There is no discount to apply."
+  end
+  end
+
+  def items
+    @cart
+  end
+
+  def void_last_transaction
+    @cart.pop
+    @total_array.pop
+    @total = @total_array.sum
+  end
+end
